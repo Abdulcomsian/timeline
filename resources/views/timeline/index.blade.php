4998e4e5e4a03a5e8ca3b218e94169f346726a31
Abdul Basit <basitawan.abdul@gmail.com>
2022-12-07 16:10:29 +0500

    changes done

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -6,7 +6,7 @@
  <input type="hidden" name="timelineid" value="{{$encryptid}}" id="timelineid">
     <div class="container">
         <div class="row">
-            <div class="col-12 text-center">
+            <div class="col-12 text-center timelineheader">
                 <h1>{{$TimeLine->name ?? 'Test TimeLine'}}</h1>
                 <p>{{isset($TimeLine->name) ? date('M d,Y - D',strtotime($TimeLine->start_date)) : '(Jan 1, 2022 - Monday) 1:00 PM'}}</p>
                 <p>{{$TimeLine->description ?? 'Test Description'}}</p>
